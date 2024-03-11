32de5515210f951d0070cc2b97a0cc554b0080d6
nosewad <dames.jacob@gmail.com>
2024-03-11 16:32:34 -0500

    #

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     std::cout << "Please enter the first letter of the mathmatical function "
-                 "you'd like to use.\n" << "The options are: Addition, "
+                 "you'd like to use."<< '\n' << "The options are: Addition, "
                  "Subtraction, Multiplication, and Division." << '\n';
 
     char choice;
